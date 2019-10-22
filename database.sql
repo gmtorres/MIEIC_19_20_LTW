0c00e9404ef2003e63ea67b975c3e52b68607cbf
Gustavo Torres <torresgustam@gmail.com>
2019-10-22 13:12:15 +0100

    Database

---
+++ database.sql
@@ -0,0 +1,65 @@
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Place;
+DROP TABLE IF EXISTS Rent;
+DROP TABLE IF EXISTS Comment;
+
+
+CREATE TABLE User(
+    userID integer PRIMARY KEY,
+    userName text NOT NULL,
+    email text NOT NULL,
+    passHash text NOT NULL,
+    age date NOT NULL,
+    phoneNumber text UNIQUE NOT NULL CHECK(length(phoneNumber) = 9)
+);
+
+
+CREATE TABLE Place(
+    placeID integer PRIMARY KEY,
+    title text NOT NULL,
+    placeDescription text NOT NULL,
+    placeAddress text NOT NULL,
+    area real NOT NULL CHECK(area > 0),
+
+    placeOwner integer NOT NULL REFERENCES User(userID)
+
+    /*
+        piscina
+        wifi
+        utilities
+        empregado limpeza
+        numero de quartos
+        preço
+        etc....
+    */
+);
+
+
+CREATE TABLE Rent(
+    rentID integer PRIMARY KEY,
+    place integer REFERENCES Place(placeID),
+    tourist integer REFERENCES User(userID),
+    price integer NOT NULL CHECK(price > 0),
+    maxCapacity integer NOT NULL CHECK(maxCapacity > 0),
+    startDate date NOT NULL,
+    endDate date NOT NULL CHECK(startDate >= endDateq)
+);
+
+
+CREATE TABLE Comment(
+    commentID integer PRIMARY KEY,
+    placeID integer NOT NULL REFERENCES Place(placeID),
+    writer integer NOT NULL REFERENCES User(userID),
+    classification integer NOT NULL CHECK (classification >= 1 AND classification <= 5),
+    title text NOT NULL,
+    comment text NOT NULL
+);
+
+INSERT INTO User(userName,email,passHash,age,phoneNumber) VALUES ("Joao" , "exemplo@gmail.com","dsdfaasf235",30,912345678); /*owner*/
+INSERT INTO User(userName,email,passHash,age,phoneNumber) VALUES ("Fernado" , "exemplo2@gmail.com","f5as12g35",19,924324346); /*tourist*/
+
+INSERT INTO Place(title,placeDescription,placeAddress,area,placeOwner) VALUES ("Nice house" , "Very good" , "Rua dfs", "Porto",1);
+
+INSERT INTO Rent(place,tourist,price,maxCapacity,startDate,endDate) VALUES (1,2,1000,3,'12-10-2019','20-10-2019');
+
+INSERT INTO Comment(placeID,writer,classification,title,comment) VALUES (1,2,4,"My stay","could improve");
