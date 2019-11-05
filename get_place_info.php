c252b392280b73ea635464870170c6bf1c9b13cc
mafaldasantos10 <anamaf13@gmail.com>
2019-11-05 13:32:52 +0000

    HomePage and HousePage started
    Co-authored-by: diogoalex01 <up201706892@fe.up.pt>
    Co-authored-by: mafaldasantos10 <up201706791@fe.up.pt>

--- get_place_info.php
+++ get_place_info.php
@@ -1,26 +1,25 @@
-<?php 
-    
-    function drawPlace ($place_id){
-        $db = new PDO('sqlite:database.db');
+<?php
 
-        $stmt = $db->prepare('Select * from Place 
+function drawPlace($place_id)
+{
+    $db = new PDO('sqlite:database.db');
+
+    $stmt = $db->prepare('Select * from Place 
                             JOIN User 
                             where user.userid = place.placeOwner 
                             and placeid = :place_id');
-        $stmt->bindParam(':place_id',$place_id);
-        $stmt->execute();
-        $places = $stmt->fetchAll();
-        
-        foreach( $places as $place) {   ?>
+    $stmt->bindParam(':place_id', $place_id);
+    $stmt->execute();
+    $place = $stmt->fetch();
 
-            <h1> <?=$place['title']?> </h1>
-            <h3> <?=$place['userName']?> </h3>
-            <h4> <?=$place['area']?> </h4>
-            <h5> <?=$place['placeAddress']?> </h5>
+    ?>
 
-            <h3> <?=$place['placeDescription']?> </h3>
-        
-        <?php   }
-        
-        }   ?>
-    
+    <h1> <?= $place['title'] ?> </h1>
+    <h3> <?= $place['userName'] ?> </h3>
+    <h4> <?= $place['area'] ?> </h4>
+    <h1> <?= $place['placeAddress'] ?> </h1>
+    <h3> <?= $place['placeDescription'] ?> </h3>
+
+<?php
+
+} ?>
